00daca7415482cb314db044841d6822b38a38ae8
haralambos9094@gmail.com <haralambos9094@gmail.com@07fd12bb-fc40-9b29-60b3-9658f3c90879>
2013-09-21 12:09:12 +0000

    Added SQL script and README file

---
+++ trunk/mobile-media-share.sql
@@ -0,0 +1,42 @@
+-- ------------------------------------------------------------------
+-- Ta kanei o diaxeirisths xrhsths postgres (psql san postgres)
+-- Xrhsths mobile-media-share
+DROP USER "mobile-media-share";
+CREATE USER "mobile-media-share" WITH PASSWORD 'medi@sh@re';
+
+-- Bash mobile-media-share
+DROP DATABASE IF EXISTS "mobile-media-share";
+CREATE DATABASE "mobile-media-share" OWNER "mobile-media-share";
+-- -------------------------------------------------------------------
+
+-- psql -h localhost -U 'mobile-media-share' -W (ta kanei o xrhsths mobile-media-share)
+-- Pinakes
+
+-- Drop ton Media prwta epeidh exei foreign key ston Users
+DROP TABLE IF EXISTS Media;
+
+-- Pinakas Eggegramenwn xrhstwn
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+	email VARCHAR(32) NOT NULL PRIMARY KEY,
+	password VARCHAR(32) NOT NULL,
+	photo VARCHAR(32),
+	name VARCHAR(32)
+);
+
+-- Pinakas Fotografiwn, videos...
+CREATE TABLE Media (
+	id VARCHAR(32) NOT NULL,
+	type VARCHAR(32) NOT NULL,
+	title VARCHAR(32) NOT NULL,
+	latitude DECIMAL(9, 6) NOT NULL,
+	longitude DECIMAL(9, 6) NOT NULL,
+	time TIMESTAMP NOT NULL, -- pote dhmiourghthike
+	"user" VARCHAR(32) NOT NULL,
+	public BOOLEAN NOT NULL, -- 'h public 'h private
+	size INT NOT NULL,
+	duration INT, -- diarkeia gia video/hxo
+	FOREIGN KEY ("user") REFERENCES Users(email) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
