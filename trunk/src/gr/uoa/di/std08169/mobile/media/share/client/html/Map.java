737f2f1943b07ae96d13775efc5bb7f6ed48c4f4
haralambos9094@gmail.com <haralambos9094@gmail.com@07fd12bb-fc40-9b29-60b3-9658f3c90879>
2013-10-07 20:45:59 +0000

    Fixed i18n, URLs as messages and login redirect

--- trunk/src/gr/uoa/di/std08169/mobile/media/share/client/html/Map.java
+++ trunk/src/gr/uoa/di/std08169/mobile/media/share/client/html/Map.java
@@ -1,17 +1,26 @@
 package gr.uoa.di.std08169.mobile.media.share.client.html;
 
+import gr.uoa.di.std08169.mobile.media.share.client.i18n.MobileMediaShareConstants;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.i18n.client.LocaleInfo;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Map implements EntryPoint, RequestCallback {
-
+	private static final MobileMediaShareConstants MOBILE_MEDIA_SHARE_CONSTANTS = 
+			GWT.create(MobileMediaShareConstants.class);
+	private static final MobileMediaShareUrls MOBILE_MEDIA_SHARE_URLS = 
+			GWT.create(MobileMediaShareUrls.class);
+	
 	@Override
 	public void onModuleLoad() {
 		try {
@@ -20,20 +29,37 @@ public class Map implements EntryPoint, RequestCallback {
 			new RequestBuilder(RequestBuilder.GET, "./login").sendRequest(null, this);
 		} catch (final RequestException _) {
 			//otidhpote paei strava, xana gurnaei stin login
-			Window.Location.assign("./login.html");
+			//url pou theloume na mas paei
+			Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.login(
+					//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+					URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName()),
+					//kwdikopoieitai to url map epeidh pernaei san parametros (meta apo ?)
+					URL.encodeQueryString(MOBILE_MEDIA_SHARE_URLS.map(
+							URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())))));
 		}
 	}
 
 	@Override
 	public void onError(final Request _, final Throwable __) {
-		Window.Location.assign("./login.html");
+		Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.login(
+				//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+				URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName()),
+				//kwdikopoieitai to url map epeidh pernaei san parametros (meta apo ?)
+				URL.encodeQueryString(MOBILE_MEDIA_SHARE_URLS.map(
+						URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())))));
 	}
 
 	//molis phre epituxws tin apantish
 	@Override
 	public void onResponseReceived(final Request request, final Response response) {
+		//an den einai logged in o xrhsths
 		if ((response.getStatusCode() != 200) || (response.getText().isEmpty()))
-			Window.Location.assign("./login.html");
-		RootPanel.get().add(new Label("Edw tha bei enas Xarths"));
+			Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.login(
+					//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+					URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName()),
+					//kwdikopoieitai to url map epeidh pernaei san parametros (meta apo ?)
+					URL.encodeQueryString(MOBILE_MEDIA_SHARE_URLS.map(
+							URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())))));
+		RootPanel.get().add(new Label(MOBILE_MEDIA_SHARE_CONSTANTS.placeMapHere()));
 	}
 }
--- trunk/src/gr/uoa/di/std08169/mobile/media/share/client/html/NewUser.java
+++ trunk/src/gr/uoa/di/std08169/mobile/media/share/client/html/NewUser.java
@@ -6,6 +6,8 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.i18n.client.LocaleInfo;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
@@ -14,6 +16,8 @@ import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
+import gr.uoa.di.std08169.mobile.media.share.client.i18n.MobileMediaShareConstants;
+import gr.uoa.di.std08169.mobile.media.share.client.i18n.MobileMediaShareMessages;
 import gr.uoa.di.std08169.mobile.media.share.client.services.UserService;
 import gr.uoa.di.std08169.mobile.media.share.client.services.UserServiceAsync;
 
@@ -21,6 +25,12 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 	//H create dhmiougrei dunamika resource (instance) tupou UserService
 	//Sundeei ta duo interfaces kai paragei ena service
 	private static final UserServiceAsync USER_SERVICE = GWT.create(UserService.class);
+	private static final MobileMediaShareConstants MOBILE_MEDIA_SHARE_CONSTANTS = 
+			GWT.create(MobileMediaShareConstants.class);
+	private static final MobileMediaShareMessages MOBILE_MEDIA_SHARE_MESSAGES = 
+			GWT.create(MobileMediaShareMessages.class);
+	private static final MobileMediaShareUrls MOBILE_MEDIA_SHARE_URLS =
+			GWT.create(MobileMediaShareUrls.class);
 	
 	private final TextBox email;
 	private final PasswordTextBox password;
@@ -36,12 +46,12 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 		password.addKeyUpHandler(this);
 		password2 = new PasswordTextBox();
 		password2.addKeyUpHandler(this);
-		ok = new Button("OK");
+		ok = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.ok());
 		ok.addClickHandler(this);
 		ok.setEnabled(false);
-		reset = new Button("Reset");
+		reset = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.reset());
 		reset.addClickHandler(this);
-		cancel = new Button("Cancel");
+		cancel = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.cancel());
 		cancel.addClickHandler(this);
 	}
 
@@ -49,7 +59,7 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 	public void onClick(final ClickEvent clickEvent) {
 		if (clickEvent.getSource() == ok) {
 			if (!password.getValue().equals(password2.getValue())) {
-				Window.alert("Ta passwords den einai idia!");
+				Window.alert(MOBILE_MEDIA_SHARE_CONSTANTS.passwordsDoNotMatch());
 				password.setValue(null);
 				password2.setValue(null);
 				ok.setEnabled(false);
@@ -59,16 +69,20 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 				//Kaleitai molis petaxei exception h emailExists
 				@Override
 				public void onFailure(final Throwable throwable) {
-					Window.alert("Den boresa na Dhmiourghsw xrhsth!");
-					Window.Location.assign("./map.html");
+					Window.alert(MOBILE_MEDIA_SHARE_MESSAGES.errorCreatingUser(throwable.getMessage()));
+					Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.map(
+							//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+							URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())));
 				}
 				
 				@Override
 				public void onSuccess(final Boolean result) {
 					if (result)
-						Window.Location.assign("./map.html");
+						Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.map(
+								//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+								URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())));
 					else {
-						Window.alert("O xrhsths uparxei hdh!");
+						Window.alert(MOBILE_MEDIA_SHARE_MESSAGES.userAlreadyExists(email.getValue()));
 						email.setValue(null);
 						password.setValue(null);
 						password2.setValue(null);
@@ -83,7 +97,9 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 			ok.setEnabled(false);
 		} else if (clickEvent.getSource() == cancel) {
 			//paei stin ketrikh selida (map.html)
-			Window.Location.assign("./map.html");
+			Window.Location.assign(MOBILE_MEDIA_SHARE_URLS.map(
+					//encodeQueryString: Kwdikopoiei to localeName san parametro gia queryString enos url
+					URL.encodeQueryString(LocaleInfo.getCurrentLocale().getLocaleName())));
 		}
 	}
 	
@@ -98,11 +114,11 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 	
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().add(new Label("Email"));
+		RootPanel.get().add(new Label(MOBILE_MEDIA_SHARE_CONSTANTS.email()));
 		RootPanel.get().add(email);
-		RootPanel.get().add(new Label("Password"));
+		RootPanel.get().add(new Label(MOBILE_MEDIA_SHARE_CONSTANTS.password()));
 		RootPanel.get().add(password);
-		RootPanel.get().add(new Label("Retype Password"));
+		RootPanel.get().add(new Label(MOBILE_MEDIA_SHARE_CONSTANTS.confirmPassword()));
 		RootPanel.get().add(password2);
 		RootPanel.get().add(new Label());
 		RootPanel.get().add(ok);
