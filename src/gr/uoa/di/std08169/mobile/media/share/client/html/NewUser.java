4052dccac14cb1075a329eacf13626331762d934
haralambos9094@gmail.com <haralambos9094@gmail.com@07fd12bb-fc40-9b29-60b3-9658f3c90879>
2014-04-25 19:02:19 +0000

    Fixed UI

--- src/gr/uoa/di/std08169/mobile/media/share/client/html/NewUser.java
+++ src/gr/uoa/di/std08169/mobile/media/share/client/html/NewUser.java
@@ -2,28 +2,35 @@ package gr.uoa.di.std08169.mobile.media.share.client.html;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.i18n.client.LocaleInfo;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 import gr.uoa.di.std08169.mobile.media.share.client.i18n.MobileMediaShareConstants;
 import gr.uoa.di.std08169.mobile.media.share.client.i18n.MobileMediaShareMessages;
 import gr.uoa.di.std08169.mobile.media.share.client.services.user.UserService;
 import gr.uoa.di.std08169.mobile.media.share.client.services.user.UserServiceAsync;
 
-public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
+public class NewUser extends Composite implements ClickHandler, EntryPoint, KeyUpHandler {
+	
+	protected static interface NewUserUiBinder extends UiBinder<Widget, NewUser>{}
+	
+	private static final NewUserUiBinder NEW_USER_UI_BINDER = 
+			GWT.create(NewUserUiBinder.class);
+	
 	//H create dhmiougrei dunamika resource (instance) tupou UserService
 	//Sundeei ta duo interfaces kai paragei ena service
 	private static final UserServiceAsync USER_SERVICE = GWT.create(UserService.class);
@@ -33,47 +40,28 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 			GWT.create(MobileMediaShareMessages.class);
 	private static final MobileMediaShareUrls MOBILE_MEDIA_SHARE_URLS =
 			GWT.create(MobileMediaShareUrls.class);
-	private static final int TOP_STEP = 30;
-	private static final int LEFT_OFFSET = 360;
-	private static final int LEFT_STEP = 100;
 	
-	private final TextBox email;
-	private final PasswordTextBox password;
-	private final PasswordTextBox password2;
-	private final Button ok;
-	private final Button reset;
-	private final Button cancel;
+	@UiField
+	protected TextBox email;
+	@UiField
+	protected PasswordTextBox password;
+	@UiField
+	protected PasswordTextBox password2;
+	@UiField
+	protected Button ok;
+	@UiField
+	protected Button reset;
+	@UiField
+	protected Button cancel;
 	
 	public NewUser() {
-		int i = 0;
-		email = new TextBox();
-		email.getElement().addClassName("field");
-		email.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
+		initWidget(NEW_USER_UI_BINDER.createAndBindUi(this));
 		email.addKeyUpHandler(this);
-		password = new PasswordTextBox();
-		password.getElement().addClassName("field");
-		password.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
 		password.addKeyUpHandler(this);
-		password2 = new PasswordTextBox();
-		password2.getElement().addClassName("field");
-		password2.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
 		password2.addKeyUpHandler(this);
-		int j = 0;
-		ok = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.ok());
-		ok.getElement().addClassName("newUserButtons");
-		ok.getElement().setAttribute("style", 
-				"top: " + (TOP_STEP * i) + "px; left: " + (LEFT_OFFSET + LEFT_STEP * j++) + "px;");
 		ok.addClickHandler(this);
 		ok.setEnabled(false);
-		reset = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.reset());
-		reset.getElement().addClassName("newUserButtons");
-		reset.getElement().setAttribute("style", 
-				"top: " + (TOP_STEP * i) + "px; left: " + (LEFT_OFFSET + LEFT_STEP * j++) + "px;");
 		reset.addClickHandler(this);
-		cancel = new Button(MOBILE_MEDIA_SHARE_CONSTANTS.cancel());
-		cancel.getElement().addClassName("newUserButtons");
-		cancel.getElement().setAttribute("style", 
-				"top: " + (TOP_STEP * i) + "px; left: " + (LEFT_OFFSET + LEFT_STEP * j++) + "px;");
 		cancel.addClickHandler(this);
 	}
 
@@ -136,33 +124,6 @@ public class NewUser implements ClickHandler, EntryPoint, KeyUpHandler {
 	
 	@Override
 	public void onModuleLoad() {
-		Document.get().getBody().addClassName("bodyClass");
-//		Document.get().getBody().appendChild(Header.newHeader());
-		final FlowPanel flowPanel = new FlowPanel();
-		flowPanel.getElement().addClassName("newUser");
-		int i = 0;
-		final InlineLabel emailLabel = new InlineLabel(MOBILE_MEDIA_SHARE_CONSTANTS.email());
-		emailLabel.getElement().addClassName("label");
-		emailLabel.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
-		flowPanel.add(emailLabel);
-		flowPanel.add(email);
-		flowPanel.getElement().appendChild(Document.get().createBRElement()); //<br />
-		final InlineLabel passwordLabel = new InlineLabel(MOBILE_MEDIA_SHARE_CONSTANTS.password());
-		passwordLabel.getElement().addClassName("label");
-		passwordLabel.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
-		flowPanel.add(passwordLabel);
-		flowPanel.add(password);
-		flowPanel.getElement().appendChild(Document.get().createBRElement()); //<br />
-		final InlineLabel confirmPasswordLabel = new InlineLabel(MOBILE_MEDIA_SHARE_CONSTANTS.confirmPassword());
-		confirmPasswordLabel.getElement().addClassName("label");
-		confirmPasswordLabel.getElement().setAttribute("style", "top: " + (TOP_STEP * (i++)) + "px;");
-		flowPanel.add(confirmPasswordLabel);
-		flowPanel.add(password2);
-		flowPanel.getElement().appendChild(Document.get().createBRElement()); //<br />
-		flowPanel.add(ok);
-		flowPanel.add(reset);
-		flowPanel.add(cancel);
-		
-		RootPanel.get().add(flowPanel);
+		RootPanel.get().add(this);
 	}
 }
