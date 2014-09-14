e9bd043506e65e95e7e7ee2ef370692b6ca3576f
haralambos9094@gmail.com <haralambos9094@gmail.com@07fd12bb-fc40-9b29-60b3-9658f3c90879>
2014-09-14 15:31:42 +0000

    Fixed Map and edit media

--- trunk/android/mobile-media-share/src/main/java/gr/uoa/di/std08169/mobile/media/share/android/Upload.java
+++ trunk/android/mobile-media-share/src/main/java/gr/uoa/di/std08169/mobile/media/share/android/Upload.java
@@ -33,80 +33,103 @@ import gr.uoa.di.std08169.mobile.media.share.android.FileExplorer.FileChooser;
 public class Upload extends MobileMediaShareActivity implements GoogleMap.OnMapClickListener, TextWatcher,
         View.OnClickListener {
 
-    public static final float GOOGLE_MAPS_ZOOM = 8.0f;
-    public static final double GOOGLE_MAPS_LATITUDE = 37.968546;	//DIT lat
-    public static final double GOOGLE_MAPS_LONGITUDE = 23.766968;	//DIT lng
     private static final int REQUEST_PATH = 1;
-    private static final float MARKER_ANCHOR_X = 0.5f;
-    private static final float MARKER_ANCHOR_Y = 1.0f;
     private static final BigDecimal DEGREES_BASE = new BigDecimal(60);
 
-    private EditText fileName;
+    private EditText file;
+    private Button browse;
     private EditText title;
     private CheckBox isPublic;
     private TextView latlng;
-    private Button browseButton;
     private Button ok;
     private Button reset;
-
+    private Marker marker;
+    private ProgressDialog progress;
     private String currentFileName;
     private String absolutePath;
 
-    private ProgressDialog progress;
+    //TextChangedListener
+    @Override
+    public void afterTextChanged(final Editable editable) { // TODO
+        enableOkReset();
+    }
 
-    private Marker marker;
-    private String latitude;
-    private String longitude;
+    //TextChangedListener
+    @Override
+    public void beforeTextChanged(final CharSequence charSequence, final int start, final int count, final int after) {}
 
+    public void browse(final View view) {
+        Intent fileChooserIntent = new Intent(this, FileChooser.class);
+        startActivityForResult(fileChooserIntent, REQUEST_PATH);
+    }
+
+    // Listen for results. apo to activity fileChooser
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        // See which child activity is calling us back.
+        if ((requestCode == REQUEST_PATH) && (resultCode == RESULT_OK)) {
+            currentFileName = data.getStringExtra("GetFileName");
+            absolutePath = data.getStringExtra("GetPath") + File.separator + currentFileName;
+            file.setText(currentFileName); // TODO
+            Log.d("--> Absolute Path: ", absolutePath);
+            enableOkReset();
+        }
+    }
+
+    //OnClickListener
+    @Override
+    public void onClick(View view) { // TODO
+        if (view == file) {
+            browse(view);
+        } else if (view == browse) {
+            browse(view);
+        } else if (view == isPublic) {
+            enableOkReset();
+        } else if (view == ok) {
+            upload();
+        } else if (view == reset) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.upload);
-
-        fileName = (EditText) findViewById(R.id.title);
-        fileName.setOnClickListener(this);
-
+        file = (EditText) findViewById(R.id.file);
+        file.setOnClickListener(this);
+        browse = (Button) findViewById(R.id.browse);
+        browse.setOnClickListener(this);
         title = (EditText) findViewById(R.id.title);
         title.addTextChangedListener(this);
-
         isPublic = (CheckBox) findViewById(R.id.isPublic);
-        isPublic.setOnClickListener(this);
-
         latlng = (TextView) findViewById(R.id.latlng);
-
-        browseButton = (Button) findViewById(R.id.browseButton);
-        browseButton.setOnClickListener(this);
-
         ok = (Button) findViewById(R.id.ok);
+        ok.setEnabled(false);
         ok.setOnClickListener(this);
-
         reset = (Button) findViewById(R.id.reset);
+        reset.setEnabled(false);
         reset.setOnClickListener(this);
-
         try {
             MapsInitializer.initialize(getApplicationContext());
             final GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(GOOGLE_MAPS_LATITUDE, GOOGLE_MAPS_LONGITUDE), GOOGLE_MAPS_ZOOM));
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Map.GOOGLE_MAPS_LATITUDE, Map.GOOGLE_MAPS_LONGITUDE), Map.GOOGLE_MAPS_ZOOM));
             //prosthikh marker ston xarth me tis default suntetagmenes
             marker = map.addMarker(new MarkerOptions().
                     icon(BitmapDescriptorFactory.fromResource(R.drawable.upload_marker)).
-                    anchor(MARKER_ANCHOR_X, MARKER_ANCHOR_Y).
-                    position(new LatLng(GOOGLE_MAPS_LATITUDE, GOOGLE_MAPS_LONGITUDE)));
+                    anchor(Map.MARKER_ANCHOR_X, Map.MARKER_ANCHOR_Y).
+                    position(new LatLng(Map.GOOGLE_MAPS_LATITUDE, Map.GOOGLE_MAPS_LONGITUDE)));
             map.setOnMapClickListener(this);
-            latlng.setText("(" + formatLatitude(new BigDecimal(GOOGLE_MAPS_LATITUDE)) + ", " +
-                    formatLongitude(new BigDecimal(GOOGLE_MAPS_LONGITUDE)) +")");
-            this.latitude = String.valueOf(GOOGLE_MAPS_LATITUDE);
-            this.longitude = String.valueOf(GOOGLE_MAPS_LONGITUDE);
+            latlng.setText("(" + formatLatitude(new BigDecimal(Map.GOOGLE_MAPS_LATITUDE)) + ", " +
+                    formatLongitude(new BigDecimal(Map.GOOGLE_MAPS_LONGITUDE)) +")"); // TODO
         } catch (GooglePlayServicesNotAvailableException e) {
-            error(R.string.errorRetrievingMedia, "error loading Google Maps");
+            error(R.string.errorRetrievingMedia, e.getMessage());
         }
-
         progress = new ProgressDialog(this);
         progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progress.setIndeterminate(true);
-        progress.setMessage("Waiting...");
+        progress.setTitle("Uploading File"); // TODO
+        progress.setMessage("Waiting..."); // TODO
     }
 
     @Override
@@ -116,84 +139,37 @@ public class Upload extends MobileMediaShareActivity implements GoogleMap.OnMapC
         return true;
     }
 
+    //OnMapClickListener
+    @Override
+    public void onMapClick(final LatLng latLng) {
+        latlng.setText("(" + formatLatitude(new BigDecimal(latLng.latitude)) + ", " +
+                formatLongitude(new BigDecimal(latLng.longitude)) +")");
+        marker.setPosition(latLng);
+        enableOkReset();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void getfile(final View view){
-        Intent fileChooserIntent = new Intent(this, FileChooser.class);
-        startActivityForResult(fileChooserIntent, REQUEST_PATH);
-    }
-
-    // Listen for results. apo to activity fileChooser
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        // See which child activity is calling us back.
-        if (requestCode == REQUEST_PATH){
-            if (resultCode == RESULT_OK) {
-                currentFileName = data.getStringExtra("GetFileName");
-                absolutePath = data.getStringExtra("GetPath") + File.separator + currentFileName;
-                fileName.setText(currentFileName);
-Log.d("--> Absolute Path: ", absolutePath);
-            }
-        }
-    }
-
-    //OnClickListener
-    @Override
-    public void onClick(View view) {
-        if (view == fileName) {
-            getfile(view);
-        } else if (view == browseButton) {
-            getfile(view);
-        } else if (view == ok) {
-            upload();
-        } else if (view == reset) {
-
-        }
+        return (item.getItemId() == R.id.settings) || super.onOptionsItemSelected(item);
     }
 
     //TextChangedListener
     @Override
-    public void beforeTextChanged(final CharSequence charSequence, final int start, final int count, final int after) {
+    public void onTextChanged(final CharSequence charSequence, final int start, final int count, final int after) {}
 
-    }
+    private void enableOkReset() {
 
-    //TextChangedListener
-    @Override
-    public void onTextChanged(final CharSequence charSequence, final int start, final int count, final int after) {
-
-    }
-
-    //TextChangedListener
-    @Override
-    public void afterTextChanged(Editable editable) {
-
-    }
-
-    //OnMapClickListener
-    @Override
-    public void onMapClick(final LatLng latLng) {
-        latlng.setText("(" + formatLatitude(new BigDecimal(latLng.latitude)) + ", " +
-            formatLongitude(new BigDecimal(latLng.longitude)) +")");
-        marker.setPosition(latLng);
-        this.latitude = String.valueOf(marker.getPosition().latitude);
-        this.longitude = String.valueOf(marker.getPosition().longitude);
     }
 
     private void upload() {
         final String absoluteFilePath = absolutePath;
         final String title = this.title.getText().toString();
         final boolean publik = (isPublic.isChecked()) ? true : false;
-        final String latitude = this.latitude;
-        final String longitude = this.longitude;
+//        final String latitude = this.latitude; //TODO
+//        final String longitude = this.longitude; //TODO
 
         /* Process Bar */
         progress.show();
@@ -240,7 +216,7 @@ Log.d("--> Absolute Path: ", absolutePath);
 //        }.execute(absoluteFilePath, title, Boolean.toString(publik), latitude, longitude);
     }
 
-    public static String formatLatitude(final BigDecimal latitude) {
+    public static String formatLatitude(final BigDecimal latitude) { // TODO
         // 1 moira = 60 prwta lepta
         // 1 prwto lepto = 60 deutera
         // to divideAndRemainder kanei tin diairesh kai epistefei:
@@ -257,7 +233,7 @@ Log.d("--> Absolute Path: ", absolutePath);
                 degrees + "° " + minutes + "′" + seconds + "″ S";
     }
 
-    public static String formatLongitude(final BigDecimal longitude) {
+    public static String formatLongitude(final BigDecimal longitude) { // TODO
         final BigDecimal[] temp1 = longitude.multiply(DEGREES_BASE).multiply(DEGREES_BASE).divideAndRemainder(DEGREES_BASE);
         final int seconds = temp1[1].intValue();
         final BigDecimal[] temp2 = temp1[0].divideAndRemainder(DEGREES_BASE);
